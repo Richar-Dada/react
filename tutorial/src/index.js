b2bfac1bd629a599c542c0cd6cc011ff3d88a3de
zicong <zicong@Igola.com>
2017-10-24 08:57:02 +0800

    Push  tutorial part2

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
-import Square from './components/square.js'
+import Board from './components/board.js'
 
 ReactDom.render(
-	<Square value="click" />,
+	<Board />,
 	document.getElementById('root')
 	)
